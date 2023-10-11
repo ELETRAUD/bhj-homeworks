c024c74f1dfbe5cfda68247928a27e7dbe2e5841
ELETRAUD <122927068+ELETRAUD@users.noreply.github.com>
2023-10-11 17:19:21 +0300

    Update task.js

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,41 @@
+let cookieImg;
+let clickerCount;
+let clickerSpeed;
+let startTime;
+let counter;
+ 
+function truncated(num, digit = 3) {
+    const divider = 10 ** digit;
+    return Math.round(num * divider) / divider;
+}
+ 
+function gameInit(params) {
+    cookieImg = document.querySelector(params.cookieImg);
+    clickerCount = document.querySelector(params.clickerCount);
+    clickerSpeed = document.querySelector(params.clickerSpeed);
+ 
+    startTime = new Date();
+    counter = 0;
+ 
+    cookieImg.addEventListener('click', gameClick);
+}
+ 
+function gameClick() {
+    console.log(counter);
+    counter += 1;
+    const speed = counter / (new Date() - startTime) * 1000;
+ 
+    clickerCount.textContent = counter;
+    clickerSpeed.textContent = truncated(speed);
+    cookieImg.classList.toggle('clicker__cookie-second_state')
+}
+ 
+window.addEventListener('load', function () {
+    const params = {
+        cookieImg: '#cookie',
+        clickerCount: '#clickerCount',
+        clickerSpeed: '#clickerSpeed',
+    };
+    gameInit(params);
+});
+ 
