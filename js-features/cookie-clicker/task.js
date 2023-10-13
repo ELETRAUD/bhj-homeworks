06ea994f9a5958f35dde30b72e82cdd2b6da425e
ELETRAUD <122927068+ELETRAUD@users.noreply.github.com>
2023-10-13 17:52:36 +0300

    Update task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,41 +1,26 @@
-let cookieImg;
-let clickerCount;
-let clickerSpeed;
-let startTime;
-let counter;
+const cookie = document.getElementById('cookie');
+const counter = document.getElementById('clicker__counter');
+
+
+let count = 0;
+
+
+function handleClick() {
+  
+  count++;
+  
  
-function truncated(num, digit = 3) {
-    const divider = 10 ** digit;
-    return Math.round(num * divider) / divider;
+  counter.textContent = count;
+  
+ 
+  if (cookie.width === 200) {
+    cookie.width = 180;
+    cookie.height = 180;
+  } else {
+    cookie.width = 200;
+    cookie.height = 200;
+  }
 }
- 
-function gameInit(params) {
-    cookieImg = document.querySelector(params.cookieImg);
-    clickerCount = document.querySelector(params.clickerCount);
-    clickerSpeed = document.querySelector(params.clickerSpeed);
- 
-    startTime = new Date();
-    counter = 0;
- 
-    cookieImg.addEventListener('click', gameClick);
-}
- 
-function gameClick() {
-    console.log(counter);
-    counter += 1;
-    const speed = counter / (new Date() - startTime) * 1000;
- 
-    clickerCount.textContent = counter;
-    clickerSpeed.textContent = truncated(speed);
-    cookieImg.classList.toggle('clicker__cookie-second_state')
-}
- 
-window.addEventListener('load', function () {
-    const params = {
-        cookieImg: '#cookie',
-        clickerCount: '#clickerCount',
-        clickerSpeed: '#clickerSpeed',
-    };
-    gameInit(params);
-});
- 
+
+
+cookie.addEventListener('click', handleClick);
